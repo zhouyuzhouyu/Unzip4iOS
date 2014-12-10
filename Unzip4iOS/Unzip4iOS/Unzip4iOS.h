37522ac0595d44f06d85789238fd29b8f63a00c0
zoyu <524112470@qq.com>
2014-12-10 16:49:36 +0800

    init

---
+++ Unzip4iOS/Unzip4iOS/Unzip4iOS.h
@@ -0,0 +1,20 @@
+//
+//  Unzip4iOS.h
+//  Unzip4iOS
+//
+//  Created by zoyu on 14/12/3.
+//  Copyright (c) 2014年 zoyu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <Unzip4iOS/ZipArchive.h>
+
+//! Project version number for Unzip4iOS.
+FOUNDATION_EXPORT double Unzip4iOSVersionNumber;
+
+//! Project version string for Unzip4iOS.
+FOUNDATION_EXPORT const unsigned char Unzip4iOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Unzip4iOS/PublicHeader.h>
+
+
---
+++ Unzip4iOS/Unzip4iOSTests/Unzip4iOSTests.m
@@ -0,0 +1,40 @@
+//
+//  Unzip4iOSTests.m
+//  Unzip4iOSTests
+//
+//  Created by zoyu on 14/12/3.
+//  Copyright (c) 2014年 zoyu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+@interface Unzip4iOSTests : XCTestCase
+
+@end
+
+@implementation Unzip4iOSTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    XCTAssert(YES, @"Pass");
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
